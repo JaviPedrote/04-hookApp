3b24dc1623d6c9074188be0aa3533eb6068bdc1d
JaviPedrote <pedrote3.1416gmail.com>
2025-01-04 12:55:43 +0100

    useContext

---
+++ src/08-useContext/context/UserProvider.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { UserContext } from "./userContext";
+import { useState } from "react";
+
+export const UserProvider = ({ children }) => {
+
+  const [user, setUser] = useState();
+
+  return (
+      // <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+      <UserContext.Provider value={{ user, setUser }}>
+          { children }
+      </UserContext.Provider>
+  )
+}
